a2ae395b99713af77dec890e455a43f31803b225
Rafael Garcia <rafaelgarcia2013@gmail.com>
2016-12-29 21:16:11 -0300

    Add changes

--- blog/forms.py
+++ blog/forms.py
@@ -2,8 +2,13 @@ from django import forms
 
 from .models import Post
 
-class PostForm(forms.ModelForm):
+# class PostForm(forms.ModelForm):
 
-    class Meta:
-        model = Post
-        fields = ('title', 'text', 'image')
+#     class Meta:
+#         model = Post
+#         fields = ('title', 'text', 'image')
+
+class UploadFileForm(forms.Form):
+    title = forms.CharField(max_length=50)
+    text = forms.CharField(label='Texto', widget=forms.Textarea)
+    file = forms.FileField()
--- blog/views.py
+++ blog/views.py
@@ -12,9 +12,24 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
+# def post_new(request):
+#     if request.method == "POST":
+#         form = PostForm(request.POST)
+#         if form.is_valid():
+#             post = form.save(commit=False)
+#             post.published_date = timezone.now()
+#             if request.user is User:
+#                 post.author = request.user
+#             post.save()
+#             return redirect('post_detail', pk=post.pk)
+#     else:
+#         form = PostForm()
+#     return render(request, 'blog/post_edit.html', {'form': form})
+# Imaginary function to handle an uploaded file.
+
 def post_new(request):
-    if request.method == "POST":
-        form = PostForm(request.POST)
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
             post = form.save(commit=False)
             post.published_date = timezone.now()
@@ -23,7 +38,7 @@ def post_new(request):
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
-        form = PostForm()
+        form = UploadFileForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
 def post_edit(request, pk):
