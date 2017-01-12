cdd2688d70ec9f936f92889ee954e877ffd64f31
Rafael Garcia <rafaelgarcia2013@gmail.com>
2017-01-12 15:54:57 -0300

    Add ckeditor - post

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,8 +21,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^ckeditor/', include('ckeditor_uploader.urls')),
     url(r'', include('blog.urls'))
 ]
 
 if settings.DEBUG:
-	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
