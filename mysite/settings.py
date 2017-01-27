8b63269d704a1168f0a935e03c4909156d47f366
Rafael Garcia <rafaelgarcia2013@gmail.com>
2017-01-27 16:10:58 -0300

    Change Heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+	os.path.join(PROJECT_ROOT, 'static'),
 )
 
 # Quick-start development settings - unsuitable for production
